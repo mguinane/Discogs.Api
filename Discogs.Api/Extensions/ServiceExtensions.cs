79f545cf2309670c1333f81159264cbbc711ba1d
Marcus Guinane <marcus.guinane@gmail.com>
2021-05-18 20:44:02 +0100

    ServiceExtensions added.

---
+++ Discogs.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Discogs.Api.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discogs.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureContainer(this IServiceCollection services)
+        {
+            services.AddSingleton<IDiscogsRepository, DiscogsRepository>();
+        }
+
+        public static void ConfigureControllers(this IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddNewtonsoftJson(config =>
+                {
+                    // Allow camel casing in JSON property names
+                    config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Discogs.Api",
+                    Version = "v1",
+                    Description = "ASP.NET Core 2.0 API project to wrap Discogs API"
+                });
+            });
+        }
+    }
+}
--- Discogs.Api/Startup.cs
+++ Discogs.Api/Startup.cs
@@ -1,11 +1,9 @@
-﻿using Discogs.Api.Data;
+﻿using Discogs.Api.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Newtonsoft.Json.Serialization;
 
 namespace Discogs.Api
 {
@@ -18,31 +16,16 @@ namespace Discogs.Api
 
         // TODO how to read appsettings file?
 
-        // TODO move service config to extension methods, add CORS support
+        // TODO add CORS support?
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IDiscogsRepository, DiscogsRepository>();
-
-            services.AddControllers()
-                .AddNewtonsoftJson(config =>
-                {
-                    // Allow camel casing in JSON property names
-                    config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                });
-
-            services.AddSwaggerGen(config =>
-            {
-                config.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Discogs.Api",
-                    Version = "v1",
-                    Description = "ASP.NET Core 2.0 API project to wrap Discogs API"
-                });
-            });
+            services.ConfigureContainer();
+            services.ConfigureControllers();
+            services.ConfigureSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
